9084066007dbd6d52c99b3c09c604cd46c4bf471
shulin59 <shulin59@gmail.com>
2020-11-17 13:06:56 +0530

    media queries add

--- src/component/Project.js
+++ src/component/Project.js
@@ -25,7 +25,7 @@ export default class Project extends Component{
                                     <i><b>Experimental Validation of an IoT based Device Selective Power Cut Mechanism Using Power Line Carrier Communication for Smart Management of Electricity</b></i><br/>
                                     This work was dealt with Implementing a device selective protocol which can selectively cut power of particular devices depending upon their power rating, 
                                     manufacturer company etc. by use of power line carrier communication. Hence revolutionizing the concept of area selective power cut to device selective power cut.</p><br/>
-                                    <a className="btn downloadbtn rounded-pill shadow" href="https://link.springer.com/article/10.1007/s42835-020-00568-6#:~:text=With%20device%20selective%20power%2Dcut,with%20limited%20use%20of%20electricity" 
+                                    <a className="btn viewbtn rounded-pill shadow" href="https://link.springer.com/article/10.1007/s42835-020-00568-6#:~:text=With%20device%20selective%20power%2Dcut,with%20limited%20use%20of%20electricity" 
                                         target="_blank" style={{textDecoration:'none', width:'50%'}}>
                                         <i className="fa fa-eye">&ensp;View Research Paper</i>
                                     </a>                                
@@ -43,7 +43,7 @@ export default class Project extends Component{
                                 <h5 class="card-title">Vehicle Monitoring System (VEMS)</h5>
                                 <p class="card-text" style={{lineHeight:'2.0', textAlign:'justify', fontSize:"17px"}}>VeMS is to track and monitor the vehicle parameters using IoT. 
                                 The parameter being object detection, weather forecast, engine performance and car location.</p>
-                                <a className="btn downloadbtn rounded-pill shadow" href={vems} target="_blank" style={{textDecoration:'none', width:'50%', marginTop:'68px'}}>
+                                <a className="btn viewbtn rounded-pill shadow" href={vems} target="_blank" style={{textDecoration:'none', width:'50%', marginTop:'68px'}}>
                                     <i className="fa fa-eye">&ensp;View</i>
                                 </a>
                             </div>
@@ -54,7 +54,7 @@ export default class Project extends Component{
                                 <h5 class="card-title">Facial Recognition Smart Lock</h5><br/>
                                 <p class="card-text" style={{lineHeight:'2.0', textAlign:'justify', fontSize:"17px"}}>The main purpose of the project is to improve security systems in homes, offices etc. 
                                 The idea is to unlock a door by recognizing the face of an authorized person And then send the image to the cloud for analysis and to notify the user.</p>
-                                <a className="btn downloadbtn rounded-pill shadow" href={face} target="_blank" style={{textDecoration:'none', width:'50%'}}>
+                                <a className="btn viewbtn rounded-pill shadow" href={face} target="_blank" style={{textDecoration:'none', width:'50%'}}>
                                     <i className="fa fa-eye">&ensp;View</i>
                                 </a>
                             </div>
@@ -64,7 +64,7 @@ export default class Project extends Component{
                             <div class="card-body">
                                 <h5 class="card-title">Artificial Atmosphere<br/>(ARTMOS)</h5>
                                 <p class="card-text" style={{lineHeight:'2.0', textAlign:'justify', fontSize:"17px"}}>To grow crops with suitable surrounding in any environment by creating artificial conditions using micro-controller.</p>
-                                <a className="btn downloadbtn rounded-pill shadow" href={artmos} target="_blank" style={{textDecoration:'none', width:'50%', marginTop:'102px'}}>
+                                <a className="btn viewbtn rounded-pill shadow" href={artmos} target="_blank" style={{textDecoration:'none', width:'50%', marginTop:'102px'}}>
                                     <i className="fa fa-eye">&ensp;View</i>
                                 </a>
                             </div>
